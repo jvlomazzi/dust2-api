4cb18fe902d65203ccac4652ef511dab7984fe75
João Matos <41305642+jvlomazzi@users.noreply.github.com>
2023-05-06 03:22:24 -0300

    fix: change general api module to import other modules

--- src/api.module.ts
+++ src/api.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { RankingController } from './ranking/ranking.controller';
+import { RankingModule } from './ranking/ranking.module';
 
 @Module({
-  controllers: [RankingController],
+  imports: [RankingModule],
 })
 export class ApiModule {}
