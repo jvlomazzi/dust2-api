f0bf9d588ff37c8173fccef03035ea7e3cbfefab
João Matos <41305642+jvlomazzi@users.noreply.github.com>
2023-05-06 17:12:42 -0300

    chore: add env variables

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { RankingModule } from './ranking/ranking.module';
 import { ApiModule } from './api.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  const configService = new ConfigService();
   const app = await NestFactory.create(ApiModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Dust2 Brasil API')
-    .setDescription('API não oficial do site Dust2 BR')
-    .setVersion('1.0')
+    .setTitle(configService.get('API_NAME'))
+    .setDescription(configService.get('API_DESCRIPTION'))
+    .setVersion(configService.get('API_VERSION'))
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(configService.get('PORT'));
 }
 
 bootstrap();
