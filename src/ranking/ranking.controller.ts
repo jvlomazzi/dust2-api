e27ef48a3aa7685e8a532f308e558877637882bd
João Matos <41305642+jvlomazzi@users.noreply.github.com>
2023-05-06 17:50:41 -0300

    feat: add findAll to ranking endpoint

--- src/ranking/ranking.controller.ts
+++ src/ranking/ranking.controller.ts
@@ -1,14 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { RankingService } from './ranking.service';
 
 // @ApiBearerAuth()
 @ApiTags('ranking')
 @Controller('ranking')
 export class RankingController {
+  constructor(private readonly rankingService: RankingService) {}
   @Get()
   //@ApiPaginatedResponse(CatDto)
   //findAll(): Observable<{ total: number, limit: number, offset: number, results: CatDto[] }>
-  findAll() {
-    return 'Ranking';
+  async findAll() {
+    const results = await this.rankingService.findAll();
+    return results;
   }
 }
--- src/ranking/ranking.service.ts
+++ src/ranking/ranking.service.ts
@@ -1,4 +1,54 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import cheerio, { CheerioAPI } from 'cheerio';
+import { Team } from './entities/team.entity';
 
 @Injectable()
-export class RankingService {}
+export class RankingService {
+  constructor(private config: ConfigService) {}
+
+  /**
+   * TODO:
+   * add return type
+   * implement player array and lineUp attribute to response
+   */
+  async findAll() {
+    const url = `${this.config.get('BASE_URL')}/${this.config.get('RANKING')}`;
+
+    const body = await (await fetch(url)).text();
+    const $ = cheerio.load(body);
+    const results = this.bodyParser($);
+
+    return results;
+  }
+
+  private bodyParser($: CheerioAPI): Team[] {
+    const content = $('.ranking-wrapper');
+    const teams: Team[] = [];
+
+    content.map((index, element) => {
+      const ranking: number = Number(
+        $(element).find('.ranking-number').text().replace('#', ''),
+      );
+      const teamInfo = $(element)
+        .find('.ranking-team')
+        .find('.ranking-team-info');
+
+      const points: number = Number(
+        teamInfo.find('.ranking-team-points').text().replace(/\D/g, ''),
+      );
+      teamInfo.find('.ranking-team-points').remove();
+      const name = teamInfo.text();
+      const rankingChange = $(element).find('.ranking-change').text();
+
+      teams.push({
+        ranking: ranking,
+        name: name,
+        points: points,
+        rankingChange: rankingChange,
+      });
+    });
+
+    return teams;
+  }
+}
