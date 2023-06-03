d2ab6e53bf3c241e56f9d7d88ba9a4649f5fbb60
João Matos <41305642+jvlomazzi@users.noreply.github.com>
2023-06-03 19:57:56 -0300

    :hammer: add return type and fix lint

--- src/news/news.controller.ts
+++ src/news/news.controller.ts
@@ -1,15 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { NewsService } from './news.service';
+import { News } from './entities/news.entity';
 
 @ApiTags('news')
 @Controller('news')
 export class NewsController {
-    constructor(private readonly newsService: NewsService) { }
+  constructor(private readonly newsService: NewsService) {}
 
-    @Get()
-    async findAll() {
-        const results = this.newsService.findAll();
-        return results;
-    }
+  @Get()
+  async findAll(): Promise<News[]> {
+    const results = this.newsService.findAll();
+    return results;
+  }
 }
